3561ac0ff4ad695594a90795dffcfd5bc4371b98
Bùi Cao Chinh <buicaochinh0811@gmail.com>
2021-06-25 15:48:51 +0700

    clean code

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="container">
-    <Table/>
+    <Table />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import Table from "./components/Table.vue"
+import { defineComponent } from 'vue'
+import Table from './components/Table.vue'
 
 export default defineComponent({
-  name: "App",
-  components: { Table }
+  name: 'App',
+  components: { Table },
 })
 </script>
